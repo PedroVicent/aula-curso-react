6e09804f8d7b54a68ed121201aa56b199741d319
CASTBSB\pedro.bernardo <pedro.bernardo@castgroup.com.br>
2019-06-13 19:36:18 -0300

    Primeiro Commit

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { View, Text, Alert, TouchableHighlight } from "react-native";
+import { View, Text, Alert, TouchableHighlight, TouchableOpacity, ActivityIndicator } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 class HomeScreen extends React.Component {
 
   static navigationOptions = {
-    title: 'Home',
+    title: 'Cursos',
     headerStyle: {
       backgroundColor: '#f4511e',
     },
@@ -16,19 +16,47 @@ class HomeScreen extends React.Component {
   };
 
   state = {
-    value: 1
+    cursos: [],
+    isLoading: true
+  }
+
+  componentDidMount = async () => {
+    const urlCursos = 'http://104.248.133.2:7001/cursos';
+    const response = await fetch(urlCursos);
+    const responseParsead = await response.json();
+
+    this.setState({cursos: responseParsead, isLoading: false});
   }
 
   render() {
+
+    if (this.state.isLoading) {
+      return (
+        <View>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+      )
+    }
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <TouchableHighlight
-          style={{ height: 30, width: 30, backgroundColor: 'blue' }}
+      <View style={{ flex: 1 }}>
+        {this.state.cursos.map(curso => {
+          return (
+          <TouchableOpacity 
           onPress={() => this.props.navigation.navigate('Second', {
-            valorDoState: this.state.value
-          })}>
-          <Text>asuauahsa</Text>
-        </TouchableHighlight>
+            id: curso.id
+          })}
+          key={curso.nome} style={{ 
+              flexDirection: 'row', 
+              padding: 12, 
+              borderBottomColor: '#ccc', 
+              borderBottomWidth: 1, 
+              alignItems: 'center'
+              }}>
+            <Text style={{ marginLeft: 15, fontSize: 22}}>{curso.id}</Text>
+            <Text style={{ marginLeft: 15, fontSize: 22}}>{curso.nome}</Text>
+          </TouchableOpacity>
+            )
+          })}
       </View>
     );
   }
@@ -37,7 +65,7 @@ class HomeScreen extends React.Component {
 class DetailsScreen extends React.Component {
 
   static navigationOptions = {
-    title: 'Second',
+    title: 'Detalhes da Pagina',
     headerStyle: {
       backgroundColor: 'red',
     },
@@ -47,13 +75,60 @@ class DetailsScreen extends React.Component {
     },
   };
 
+  state = {
+    cursoDetalhado: [],
+    isLoading: true
+  }
+
+  componentDidMount = async () => {
+
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('id');
+
+    const urlCursoDetalhado = `http://104.248.133.2:7001/cursos/${itemId}`;
+    const response = await fetch(urlCursoDetalhado);
+    const responseParsead = await response.json();
+
+    const urlAlunosInscritos = `http://104.248.133.2:7001/cursos/${itemId}`;
+    const responseAlunos = await fetch(urlAlunosInscritos);
+    const responseParseadAlunos = await responseAlunos.json();
+
+    this.setState({cursoDetalhado: responseParsead[0], alunosCadastro: responseParseadAlunos[0], isLoading: false })
+  }
+
+
   render() {
+
+    if (this.state.isLoading) {
+      return (
+        <View>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+      )
+    }
+
     return (
-      <View>
-        <Text>Funcionando a navegação</Text>
-        <TouchableHighlight style={{ height: 30, width: 30, backgroundColor: 'blue' }} onPress={() => Alert.alert('aushuash')}>
-          <Text>asuauahsa</Text>
-        </TouchableHighlight>
+      <View style={{ flex: 1, backgroundColor: '#eee', padding: 20}}>
+        <View style={{ backgroundColor: '#fff', borderColor: '#ccc', borderWidth: 1, borderRadius: 5, padding: 12}}>
+          <Text>Id : {this.state.cursoDetalhado.id}</Text>
+          <Text>Curso : {this.state.cursoDetalhado.nome}</Text>
+          <Text>Professor : {this.state.cursoDetalhado.professor}</Text>
+          <Text>Local : {this.state.cursoDetalhado.local}</Text>
+        </View>
+
+        <View style={{ marginTop: 10, backgroundColor: '#fff', borderColor: '#ccc', borderWidth: 1, borderRadius: 5, padding: 12}}>
+        <View style={{ backgroundColor: '#bb',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 15}}>
+          <Text>ALUNOS CADASTRADOS</Text>
+        </View>
+          <Text>ALUNOS CADASTRADOS</Text>
+          <Text>Id : {this.state.cursoDetalhado.id}</Text>
+          <Text>Curso : {this.state.cursoDetalhado.nome}</Text>
+          <Text>Professor : {this.state.cursoDetalhado.professor}</Text>
+          <Text>Local : {this.state.cursoDetalhado.local}</Text>
+        </View>
       </View>
     )
   }
